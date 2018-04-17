e0ca5c71a390c6894e09d4299cb57b726defb1fc
stranger <26394973@qq.com>
2018-04-17 09:31:56 +0800

    修改费率

--- www/misc/account.py
+++ www/misc/account.py
@@ -6,84 +6,84 @@ ACCOUNTS = [
         "CHANNEL_ID": 1,                # 渠道编号
         "USERNAME": u"成都大橙科技-wx",   # 账号
         "PASSWORD": "orange20150611",   # 密码
-        "COST": 0.0022,                 # 成本费率
+        "COST": 0.0023,                 # 成本费率
         "DEFAULT_RATE": 0.0038          # 默认给商户的费率
     },
     {
-        "CHANNEL_ID": 2, 
-        "USERNAME": u"北京张波", 
-        "PASSWORD": "123456", 
-        "COST": 0.0022,
-        "DEFAULT_RATE": 0.0038 
+        "CHANNEL_ID": 2,
+        "USERNAME": u"北京张波",
+        "PASSWORD": "123456",
+        "COST": 0.0023,
+        "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 3, 
-        "USERNAME": u"长治博远-wx", 
-        "PASSWORD": "123456", 
-        "COST": 0.0022,
-        "DEFAULT_RATE": 0.0038 
+        "CHANNEL_ID": 3,
+        "USERNAME": u"长治博远-wx",
+        "PASSWORD": "123456",
+        "COST": 0.0023,
+        "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 4, 
-        "USERNAME": u"山西易道网络", 
+        "CHANNEL_ID": 4,
+        "USERNAME": u"山西易道网络",
         "PASSWORD": "yidaowang321",
         "COST": 0.003,
         "DEFAULT_RATE": 0.006
     },
     {
-        "CHANNEL_ID": 5, 
-        "USERNAME": u"九江好近-wx", 
+        "CHANNEL_ID": 5,
+        "USERNAME": u"九江好近-wx",
         "PASSWORD": "939235271264ZZM",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 6, 
-        "USERNAME": u"开封好近-wx", 
+        "CHANNEL_ID": 6,
+        "USERNAME": u"开封好近-wx",
         "PASSWORD": "xudong1983",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 7, 
-        "USERNAME": u"雁塔田鑫-wx", 
+        "CHANNEL_ID": 7,
+        "USERNAME": u"雁塔田鑫-wx",
         "PASSWORD": "024459Jz",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 8, 
-        "USERNAME": u"南京迦尔信息-wx", 
+        "CHANNEL_ID": 8,
+        "USERNAME": u"南京迦尔信息-wx",
         "PASSWORD": "123456",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 9, 
-        "USERNAME": u"珠海环宇时创-wx", 
+        "CHANNEL_ID": 9,
+        "USERNAME": u"珠海环宇时创-wx",
         "PASSWORD": "hysc1226",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 10, 
-        "USERNAME": u"武汉好近-wx", 
+        "CHANNEL_ID": 10,
+        "USERNAME": u"武汉好近-wx",
         "PASSWORD": "218998",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 11, 
-        "USERNAME": u"荆门纵横网络-wx", 
+        "CHANNEL_ID": 11,
+        "USERNAME": u"荆门纵横网络-wx",
         "PASSWORD": "zhwl666",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
     {
-        "CHANNEL_ID": 12, 
-        "USERNAME": u"潍坊好近渠道-wx", 
+        "CHANNEL_ID": 12,
+        "USERNAME": u"潍坊好近渠道-wx",
         "PASSWORD": "123456",
-        "COST": 0.0022,
+        "COST": 0.0023,
         "DEFAULT_RATE": 0.0038
     },
 ]
